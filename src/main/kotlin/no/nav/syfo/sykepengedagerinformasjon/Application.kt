0ef51234d9f522ed4de681f76324b2dd2057601a
Tanja Krasnomovets <victorandreevi4@gmail.com>
2024-06-10 14:08:11 +0200

    Add log to main

--- src/main/kotlin/no/nav/syfo/sykepengedagerinformasjon/Application.kt
+++ src/main/kotlin/no/nav/syfo/sykepengedagerinformasjon/Application.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.sykepengedagerinformasjon
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,5 +9,8 @@ class Application
 
 @Suppress("SpreadOperator") // TODO
 fun main(args: Array<String>) {
+    val log = LoggerFactory.getLogger(Application::class.java)
+    log.info("ASAS")
     runApplication<Application>(*args)
+    log.info("QWQW")
 }
