d12af2eb9c3488717594a4804cb46077475a8c6b
Tanja Krasnomovets <victorandreevi4@gmail.com>
2024-11-11 10:38:44 +0100

    Fix test

--- src/test/kotlin/no/nav/syfo/api/SykepengerMaxDateRestV1Test.kt
+++ src/test/kotlin/no/nav/syfo/api/SykepengerMaxDateRestV1Test.kt
@@ -31,6 +31,7 @@ class SykepengerMaxDateRestV1Test :
                 metric,
                 "123",
                 "123",
+                "123",
                 tokenValidationContextHolder
             ).apply {
                 this.tokenValidator = tokenValidator
