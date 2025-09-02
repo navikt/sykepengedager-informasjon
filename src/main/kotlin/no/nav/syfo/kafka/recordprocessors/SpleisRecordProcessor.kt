277c8a38edc6f60df42a1fe26535b8de13009c73
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2025-09-02 14:20:30 +0200

    Add utbet_tom for spleis records (#89)
    * Calculate utbet_tom for spleis records
    
    * Calculate utbet_tom for spleis records
    
    * Calculate utbet_tom for spleis records
    
    * Calculate utbet_tom for spleis records
    
    * Calculate utbet_tom for spleis records
    
    * Calculate utbet_tom for spleis records
    
    * Calculate utbetalt_tom for spleis records
    
    * Calculate utbetalt_tom for spleis records
    
    * Calculate utbetalt_tom for spleis records
    
    * Calculate utbetalt_tom for spleis records
    
    * Calculate utbetalt_tom for spleis records
    
    * Calculate utbetalt_tom for spleis records
    
    * Calculate utbetalt_tom for spleis records
    
    * Calculate utbetalt_tom for spleis records

--- src/main/kotlin/no/nav/syfo/kafka/recordprocessors/SpleisRecordProcessor.kt
+++ src/main/kotlin/no/nav/syfo/kafka/recordprocessors/SpleisRecordProcessor.kt
@@ -1,8 +1,9 @@
 package no.nav.syfo.kafka.recordprocessors
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.syfo.config.kafka.topicUtbetaling
 import no.nav.syfo.db.UtbetalingSpleisDAO
+import no.nav.syfo.kafka.consumers.spleis.domain.DagType
 import no.nav.syfo.kafka.consumers.spleis.domain.UTBETALING_UTBETALT
 import no.nav.syfo.kafka.consumers.spleis.domain.UTBETALING_UTEN_UTBETALING
 import no.nav.syfo.kafka.consumers.spleis.domain.UtbetalingSpleis
@@ -11,13 +12,15 @@ import no.nav.syfo.logger
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import java.time.LocalDate
 
 @Component
 class SpleisRecordProcessor(
     val sykepengedagerInformasjonKafkaService: SykepengedagerInformasjonKafkaService,
+    private val objectMapper: ObjectMapper,
 ) {
     private val log = logger()
-    private val objectMapper = jacksonObjectMapper()
+    private val sykepengedagtyper = listOf(DagType.NavDag, DagType.NavHelgDag, DagType.ArbeidsgiverperiodeDag)
 
     @Autowired
     private lateinit var utbetalingSpleisDAO: UtbetalingSpleisDAO
@@ -30,11 +33,21 @@ class SpleisRecordProcessor(
             }
         } catch (e: Exception) {
             log.error("Exception in [$topicUtbetaling]-processor: $e", e)
+            throw e
         }
     }
-
     private fun processUtbetalingSpleisEvent(utbetaling: UtbetalingSpleis) {
-        utbetalingSpleisDAO.storeSpleisUtbetaling(utbetaling)
+        val utbetaltTom = calculateUtbetaltTom(utbetaling)
+        utbetalingSpleisDAO.storeSpleisUtbetaling(utbetaling, utbetaltTom)
+        if (utbetaltTom != LocalDate.parse(utbetaling.tom)) {
+            log.info(
+                "UtbetaltTom $utbetaltTom er forskjellig fra tom ${utbetaling.tom} i utbetaling med dager: " +
+                    "${utbetaling.utbetalingsdager.joinToString()}"
+            )
+        }
         sykepengedagerInformasjonKafkaService.publishSykepengedagerInformasjonEvent(utbetaling.fødselsnummer)
     }
+
+    private fun calculateUtbetaltTom(utbetaling: UtbetalingSpleis): LocalDate? =
+        utbetaling.utbetalingsdager.filter { it.type in sykepengedagtyper }.maxOfOrNull { it.dato }
 }
