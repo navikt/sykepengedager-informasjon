30b6c8339280a383945ab50f63dd2871c5f79255
Daniel Engebretsen <daniel.engebretsen@nav.no>
2025-09-08 08:42:39 +0200

    make processRecord transactional

--- src/main/kotlin/no/nav/syfo/kafka/recordprocessors/SpleisRecordProcessor.kt
+++ src/main/kotlin/no/nav/syfo/kafka/recordprocessors/SpleisRecordProcessor.kt
@@ -9,6 +9,7 @@ import no.nav.syfo.kafka.producers.SykepengedagerInformasjonKafkaService
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class SpleisRecordProcessor(
@@ -19,6 +20,7 @@ class SpleisRecordProcessor(
     @Autowired
     private lateinit var utbetalingSpleisDAO: UtbetalingSpleisDAO
 
+    @Transactional
     fun processRecord(record: ConsumerRecord<String, String>) {
         val utbetaling = objectMapper.readValue(record.value(), UtbetalingSpleis::class.java)
         if (utbetaling.event == UTBETALING_UTBETALT || utbetaling.event == UTBETALING_UTEN_UTBETALING) {
