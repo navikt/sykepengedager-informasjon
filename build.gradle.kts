d2bbb4361380e260f7d5ecd5d0da0bc052e90a77
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-02-27 12:19:02 +0100

    Upgrade plugin.spring for to kotlin version 2.3.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 plugins {
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.3.10"
     kotlin("plugin.spring") version "2.3.10"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 }
